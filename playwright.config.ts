7bf3a5491f73f70c4a0f611cae7e7a3e9115aa1e
meltz-dev <100072260+meltz-dev@users.noreply.github.com>
2022-07-19 14:49:27 +0200

    added testDir in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
+    testDir: "./tests",
     reporter: 'list',
-    workers: 2,
     projects: [
         {
             name: 'chromium',
